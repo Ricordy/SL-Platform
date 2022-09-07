9ba783e8db903d017699ee9caaf466c323d38598
cadu <cadu@2km.com.br>
2022-09-07 10:25:24 +0100

    feature: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineMedium } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="flex max-w-4xl mx-auto my-12 gap-3">
+      <Link href="#contact">
+        <a>Contact</a>
+      </Link>
+      <Link href="/security">
+        <a>Security</a>
+      </Link>
+      <Link href="/faq">
+        <a>FAQ</a>
+      </Link>
+      <div className="social ml-auto gap-3 flex">
+        <Link href="https://twitter.com">
+          <a className="flex items-center">
+            <AiOutlineTwitter /> Twitter
+          </a>
+        </Link>
+        <Link href="https://instagram.com">
+          <a className="flex items-center">
+            <AiOutlineInstagram /> Instagram
+          </a>
+        </Link>
+        <Link href="https://medium.com">
+          <a className="flex items-center">
+            <AiOutlineMedium />
+            Medium
+          </a>
+        </Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
