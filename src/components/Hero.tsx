5bd8b6f30a6906f89036507e96ec1ed2832bd95b
cadu <cadu@2km.com.br>
2022-09-14 14:27:40 +0100

    feature: remove slogan

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,9 @@ type HeroPops = {
 const Hero: React.FC<HeroPops> = (props) => {
   return (
     <section className={props.className}>
-      <h2 className="text-3xl font-serif text-slate-500">
+      {/* <h2 className="text-3xl font-serif text-slate-500">
         &quot;It is in the solitude that legends are born.&quot;
-      </h2>
+      </h2> */}
     </section>
   );
 };
