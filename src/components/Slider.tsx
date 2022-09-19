5207a300ad0d870077f1b0cfb2d2cc41042df71c
cadu <cadu@2km.com.br>
2022-09-19 18:14:32 +0100

    feature: add working responsive slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { MdKeyboardArrowLeft } from "react-icons/md";
-import { MdKeyboardArrowRight } from "react-icons/md";
 
-const images = [{ url: "/slider/car1.png" }, { url: "/slider/car2.png" }];
+import Image from "next/image";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+const images = [{ url: "/slider/car1.jpg" }, { url: "/slider/car2.jpg" }];
 
 const Slider = () => {
   return (
     <section className="flex flex-col">
-      <div className="simpleSlider flex justify-center items-center h-60">
-        <MdKeyboardArrowLeft />
-        <h2 className="text-3xl mx-auto ">Slider</h2>
-        <MdKeyboardArrowRight />
+      <div className="simpleSlider flex justify-center items-center">
+        <Carousel>
+          {images.map((image, idx) => (
+            <div key={idx}>
+              <Image src={image.url} width={960} height={400} alt="car" />
+            </div>
+          ))}
+        </Carousel>
       </div>
     </section>
   );
