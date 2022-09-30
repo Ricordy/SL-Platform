626439fc76b958932ea42c55936f0b8348c5ebbf
cadu <cadu@2km.com.br>
2022-09-30 11:38:58 +0100

    style: show puzzle in one column on mobile

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -5,15 +5,15 @@ const Puzzle = () => {
   return (
     <section id="puzzle">
       <h2 className="text-2xl pb-6">Puzzle</h2>
-      <div className="flex gap-6 justify-between">
-        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md w-1/3">
+      <div className="flex flex-col sm:flex-row gap-2 justify-between">
+        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md md:w-1/3">
           <Link href="/my-puzzle">View My Puzzle</Link>
         </div>
-        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md w-1/3">
+        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md md:w-1/3">
           <Link href="/profile">Claim NFT Puzzle</Link>
         </div>
 
-        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md w-1/3">
+        <div className="flex flex-col items-center  justify-center bg-gray-200 p-6 rounded-md md:w-1/3">
           <div className="">Buy on Open Sea</div>
           <div className="text-xs">Available Soon</div>
         </div>
