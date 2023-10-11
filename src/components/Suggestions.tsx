8e566e0edd46c59c87fae1bc1119bce468eb04c0
Cadu <cadu@2km.com.br>
2023-10-11 08:48:05 -0300

    fix: our suggestions

--- src/components/Suggestions.tsx
+++ src/components/Suggestions.tsx
@@ -1,48 +1,75 @@
 import React from "react";
-import { CarouselItem } from "./Carousel";
+import Carousel from "./Carousel";
 import { type InvestmentProps } from "~/@types/investment";
-import { cn, getMissingInvestments } from "~/lib/utils";
+import { cn } from "~/lib/utils";
 import { useInvestments } from "~/lib/zustand";
-
-type HeroPops = {
-  className?: string;
-};
+import { type Address, useAccount } from "wagmi";
 
 interface SuggestionsProps {
   className?: string;
+  items?: InvestmentProps[];
 }
 
-const Suggestions = ({ className }: SuggestionsProps) => {
+const Suggestions = ({ className, items }: SuggestionsProps) => {
   const allInvestments = useInvestments((state) => state.investments);
   const userInvestments = useInvestments((state) => state.userInvestments);
+  const { address: walletAddress } = useAccount();
 
-  const investments = getMissingInvestments(allInvestments, userInvestments);
+  const getMissingInvestments = (allInvestments, userInv) => {
+    // Create a set of investment addresses from the userInv array
+    const userInvAddresses = new Set(
+      userInv?.map((investment) => investment.address)
+    );
+
+    // Filter the allInvestments array to get the missing investments
+    let missingInvestments = allInvestments?.filter(
+      (investment) => !userInvAddresses.has(investment.address)
+    );
+
+    // If the length of missingInvestments is less than 3, add elements from userInv
+    if (missingInvestments?.length < 3) {
+      for (
+        let i = 0;
+        missingInvestments?.length < 3 && i < userInv?.length;
+        i++
+      ) {
+        missingInvestments?.push(userInv[i]);
+      }
+    } else if (missingInvestments?.length > 3) {
+      missingInvestments = missingInvestments?.slice(0, 3);
+    }
+
+    return missingInvestments;
+  };
+
+  if (!items) items = getMissingInvestments(allInvestments, userInvestments);
+
   return (
-    <section>
-      <div className="relative z-20 mx-auto flex rounded-t-[56px] bg-black pt-[72px] text-white md:pb-[128px]">
-        <div className="mx-auto flex w-full max-w-screen-lg flex-col gap-[52px] px-6 md:px-0">
-          <h3 className="text-center text-2xl uppercase md:text-left">
-            Our suggestion for you
-          </h3>
-          <div
-            className={cn(
-              "mx-auto flex w-full flex-col gap-[52px] md:max-w-screen-lg md:flex-row",
-              className
-            )}
-          >
-            {investments.map((investment) => (
-              <CarouselItem
-                title={investment.basicInvestment.car?.basicInfo.title}
-                image={investment.basicInvestment.car?.basicInfo.cover.url}
-                price={investment.basicInvestment.totalInvestment.toString()}
-                address={investment.address}
-                level={investment.level.basicLevel.title}
-              />
-            ))}
-          </div>
-        </div>
+    <div
+      className={cn(
+        "relative z-20 mx-auto flex w-full rounded-t-[56px] bg-black pt-[72px] text-white md:pb-[128px]",
+        className
+      )}
+    >
+      <div className="mx-auto flex w-full max-w-[850px] flex-col gap-[52px]">
+        {items && (
+          <Carousel
+            id="55"
+            className="mb-6 max-w-[850px] px-6 pt-0 md:-ml-[58px] md:mb-0 md:px-0 md:pt-6"
+            title={
+              <h2 className="text-center text-2xl md:text-left">
+                Our suggestion for you
+              </h2>
+            }
+            items={items}
+            userAddress={walletAddress as Address}
+            isLevelDivided={false}
+            slidesPerView={3}
+            prevNavWhite={true}
+          />
+        )}
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,35 +13,6 @@ export const formatAddress = (address: string) => {
   return null;
 };
 
-export function getMissingInvestments(allInvestments, userInv) {
-  // Check if the inputs are valid arrays
-  if (!Array.isArray(allInvestments) || !Array.isArray(userInv)) {
-    console.error("Invalid input");
-    return [];
-  }
-
-  // Create a set of investment addresses from the userInv array
-  const userInvAddresses = new Set(
-    userInv.map((investment) => investment.address)
-  );
-
-  // Filter the allInvestments array to get the missing investments
-  let missingInvestments = allInvestments.filter(
-    (investment) => !userInvAddresses.has(investment.address)
-  );
-
-  // If the length of missingInvestments is less than 3, add elements from userInv
-  if (missingInvestments.length < 3) {
-    for (let i = 0; missingInvestments.length < 3 && i < userInv.length; i++) {
-      missingInvestments.push(userInv[i]);
-    }
-  } else if (missingInvestments.length > 3) {
-    missingInvestments = missingInvestments.slice(0, 3);
-  }
-
-  return missingInvestments;
-}
-
 export const getPuzzleCollectionIds = (level: number) => {
   let increment = 0;
   if (level == 2) {
